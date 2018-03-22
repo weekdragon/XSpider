1622369564d43ca7f7c57526354794db2e93e278
weekdragon <weekdragon@163.net>
2018-03-22 18:02:23 +0800

    添加简单前端页面.

---
+++ src/main/java/cn/weekdragon/xspider/controller/IndexController.java
@@ -0,0 +1,15 @@
+package cn.weekdragon.xspider.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("name", "world");
+		return "default/index";
+	}
+}
